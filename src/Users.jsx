d26da57b6827a75cd7c28f2eb88d498b1fe3ecc6
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-08-09 17:38:09 +0600

    Update and Delete button

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 const Users = () => {
-    const [users, setUsers] = useState([{
-        Name:"yousaf", Email: "ysf@gma", Age:20 
-    }])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+       axios.get('http://localhost:3001')
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+
+    }, [])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/deleteUser/'+id)
+        .then(res => {console.log(res)
+        window.location.reload()})
+        .catch(err => console.log(err))
+
+    }
+
   return (
     <div className="d-flex container-fluid vh-100 text-bg-primary justify-content-center align-items-center">
         <div className=' w-75 lg:w-80 text-bg-dark  rounded p-4'>
@@ -16,20 +31,22 @@ const Users = () => {
                     <th>Age</th>
                     <th>Action</th>
                 </thead>
-
                 <tbody>
                     {
-                        users.map((user) => {
-                           return <tr>
-                                <td>{user.Name}</td>
-                                <td>{user.Email}</td>
-                                <td>{user.Age}</td>
-                                <td className='d-flex p-2'>
-                                <Link to="/update" className='btn btn-success mb-4'>Update</Link>
-                                    <button>Delete</button>
-                                </td>
-                            </tr>
-                        })
+                    users.map((user) => {
+                        return <tr>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.age}</td>
+                            <td className='d-flex p-2'>
+                            
+                            <Link to={`/update/${user._id}` }className='btn btn-success mb-4'>Update</Link>
+
+                                <button className='btn btn-danger mb-4'
+                                onClick={(e) => handleDelete(user._id)}>Delete</button>
+                            </td>
+                        </tr>
+                    })
                     }
                 </tbody>
             </table>
